314c8f31fc325b76d2a657b4a7e32025874174ba
Md. Sajidur Rahman Labib <87533597+labib1910024@users.noreply.github.com>
2024-07-28 10:37:12 -0700

    Update Test-03.dart

--- Test-03.dart
+++ Test-03.dart
@@ -1,8 +1,9 @@
 void main() {
   var number = 42;
   var text = "Hello, Dart!";
-  
+  var bool = true;
   print(number.runtimeType); // prints "int"
   print(text.runtimeType); // prints "String"
+  print(bool.runtimeType);
 
 }
