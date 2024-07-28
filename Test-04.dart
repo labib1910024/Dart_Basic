a67a98d13af6cbb6a48189271407bd7ae773b375
Md. Sajidur Rahman Labib <87533597+labib1910024@users.noreply.github.com>
2024-07-28 10:52:16 -0700

    Update Test-04.dart

--- Test-04.dart
+++ Test-04.dart
@@ -1,6 +1,9 @@
 void main() {
   var list =[1,2,3,4,5];
+  var list1 = ['Labib','Galib','Ashik','Saikat'];
   print(list[0]);
+  print(list1[0]);
   print(list.runtimeType);
+  print(list1.runtimeType);
 
 }
