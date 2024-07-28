5a3c38d964ca527015d1a5cde9a2861e2fa3dcb9
Md. Sajidur Rahman Labib <87533597+labib1910024@users.noreply.github.com>
2024-07-28 11:19:33 -0700

    Create Test-05.dart

---
+++ Test-05.dart
@@ -0,0 +1,21 @@
+void main() {
+  
+  var fruits = {
+    'apple': 2,
+    'banana': 5,
+    'orange': 3
+  };
+  var vegetables = Map();
+  vegetables['carrot'] = 4;
+  vegetables['broccoli'] = 1;
+  vegetables['spinach'] = 6;
+
+  print(fruits);
+  print(vegetables);
+
+
+  print(fruits['apple']);
+  print(vegetables['carrot']);
+
+
+}
