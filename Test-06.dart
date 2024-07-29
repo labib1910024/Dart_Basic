41968ca461e486f2f240a2a5c70cee7ce244d976
Md. Sajidur Rahman Labib <87533597+labib1910024@users.noreply.github.com>
2024-07-29 12:27:13 -0700

    Update Test-06.dart

--- Test-06.dart
+++ Test-06.dart
@@ -3,5 +3,9 @@ void main() {
   var fruits = ['apple','banana','orange'];
   var result = fruits.reversed;
   print(result);
+  var result1 = fruits.isEmpty;
+  print(result1);
+  var result2 = fruits.isNotEmpty;
+  print(result2);
 
 }
