fe714403bcd22319447f0e937b5cba79bcf0588d
Md. Sajidur Rahman Labib <87533597+labib1910024@users.noreply.github.com>
2024-12-10 11:19:30 -0800

    Create Test_11.dart

---
+++ Test_11.dart
@@ -0,0 +1,14 @@
+void main()
+{
+      var myset = <String>{'Dhaka','Brahmanbaria', 'Rajshahi','Cumilla','Noakhali','Kushtia'};
+      var x = myset.elementAt(3);
+      print(x);
+      print(myset.length);
+      print(myset.isNotEmpty);
+
+      var myset1 = <int>{ 1,3,6,8,3,8};
+      var x1 = myset1.elementAt(3);
+      print(x1);
+      print(myset1.length);
+
+}
