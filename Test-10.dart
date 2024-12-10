eed6e1cdaba9b528ab5d853b12a23b00a575d8f1
Md. Sajidur Rahman Labib <87533597+labib1910024@users.noreply.github.com>
2024-12-10 11:02:06 -0800

    Create Test-10.dart

---
+++ Test-10.dart
@@ -0,0 +1,9 @@
+void main()
+{
+      var myset = <String>{'Dhaka','Brahmanbaria','Rajshahi','Dhaka'};
+      myset.add('Cumilla');
+      myset.addAll({'Noakhali','Kushtia'});
+      print(myset);
+
+
+}
